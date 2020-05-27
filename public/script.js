7e70b560732584886f561c2a0fcf08aedc6e67c1
Glitch (hello-express) <none>
2020-05-27 22:18:28 +0000

    🚂🚩 Checkpoint ./views/index.html:1853958/924 ./public/script.js:1853958/2223 ./server.js:1853958/472 ./public/index.html:1853958/4181

--- public/script.js
+++ public/script.js
@@ -1,41 +1,15 @@
-// client-side js, loaded by index.html
-// run by the browser each time the page is loaded
+// Load the MediaPipe handpose model assets.
+handpose.load().then(function(model){
+ 
+  // Pass in a video stream to the model to obtain 
+  // a prediction from the MediaPipe graph.
+  console.log("model initialized.")
+  const video = document.querySelector("video");
+  model.estimateHands(video).then(function(hands){
+    // Each hand object contains a `landmarks` property,
+    // which is an array of 21 3-D landmarks.
+    hands.forEach(hand => console.log(hand.landmarks));
+  })
 
-console.log("hello world :o");
 
-// define variables that reference elements on our page
-const dreamsList = document.getElementById("dreams");
-const dreamsForm = document.querySelector("form");
-
-// a helper function that creates a list item for a given dream
-function appendNewDream(dream) {
-  const newListItem = document.createElement("li");
-  newListItem.innerText = dream;
-  dreamsList.appendChild(newListItem);
-}
-
-// fetch the initial list of dreams
-fetch("/dreams")
-  .then(response => response.json()) // parse the JSON from the server
-  .then(dreams => {
-    // remove the loading text
-    dreamsList.firstElementChild.remove();
-  
-    // iterate through every dream and add it to our page
-    dreams.forEach(appendNewDream);
-  
-    // listen for the form to be submitted and add a new dream when it is
-    dreamsForm.addEventListener("submit", event => {
-      // stop our form submission from refreshing the page
-      event.preventDefault();
-
-      // get dream value and add it to the list
-      let newDream = dreamsForm.elements.dream.value;
-      dreams.push(newDream);
-      appendNewDream(newDream);
-
-      // reset form
-      dreamsForm.reset();
-      dreamsForm.elements.dream.focus();
-    });
-  });
+})
--- server.js
+++ server.js
@@ -6,27 +6,11 @@
 const express = require("express");
 const app = express();
 
-// our default array of dreams
-const dreams = [
-  "Find and count some sheep",
-  "Climb a really tall mountain",
-  "Wash the dishes"
-];
 
 // make all the files in 'public' available
 // https://expressjs.com/en/starter/static-files.html
 app.use(express.static("public"));
 
-// https://expressjs.com/en/starter/basic-routing.html
-app.get("/", (request, response) => {
-  response.sendFile(__dirname + "/views/index.html");
-});
-
-// send the default array of dreams to the webpage
-app.get("/dreams", (request, response) => {
-  // express helps us take JS objects and send them as JSON
-  response.json(dreams);
-});
 
 // listen for requests :)
 const listener = app.listen(process.env.PORT, () => {

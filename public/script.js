38a8d40adb22fd97d054f63be8e8303bcd8489d1
Glitch (hello-express) <none>
2020-05-27 22:30:27 +0000

    🐓☎️ Checkpoint ./public/script.js:1853958/338 ./handpose.js:1853958/18364 ./public/index.html:1853958/411

--- public/script.js
+++ public/script.js
@@ -1,15 +1,28 @@
-// Load the MediaPipe handpose model assets.
-handpose.load().then(function(model){
+// // Load the MediaPipe handpose model assets.
+// handpose.load().then(function(model){
  
-  // Pass in a video stream to the model to obtain 
-  // a prediction from the MediaPipe graph.
-  console.log("model initialized.")
-  const video = document.querySelector("video");
-  model.estimateHands(video).then(function(hands){
-    // Each hand object contains a `landmarks` property,
-    // which is an array of 21 3-D landmarks.
-    hands.forEach(hand => console.log(hand.landmarks));
-  })
+//   // Pass in a video stream to the model to obtain 
+//   // a prediction from the MediaPipe graph.
+//   console.log("model initialized.")
+//   const video = document.querySelector("video");
+//   model.estimateHands(video).then(function(hands){
+//     // Each hand object contains a `landmarks` property,
+//     // which is an array of 21 3-D landmarks.
+//     hands.forEach(hand => console.log(hand.landmarks));
+//   })
 
 
-})
+// })
+
+
+
+function setup() {
+  createCanvas(480, 480);
+  capture = createCapture(VIDEO);
+  capture.hide();
+}
+
+function draw() {
+  image(capture, 0, 0, width, width * capture.height / capture.width);
+  filter(INVERT);
+}

4b15b94512d867e91f60d494502a3a042d5348ca
Glitch (networked-hand-tf174-handv1) <none>
2020-08-17 00:55:38 +0000

    👾👾 Checkpoint ./public/sketch.js:1853958/4486 ./public/index.html:1853958/87

--- public/sketch.js
+++ public/sketch.js
@@ -19,6 +19,20 @@ var colors = [
   [255,120,160],
 ]
 
+function vertex3d(x,y,z,rx,ry,rz,dx,dy,dz,f){
+  let rotx = a=> [1,0,0,0, 0,cos(a),-sin(a),0, 0,sin(a),cos(a),0, 0,0,0,1]
+  let roty = a=> [cos(a),0,sin(a),0, 0,1,0,0, -sin(a),0,cos(a),0, 0,0,0,1]
+  let rotz = a=> [cos(a),-sin(a),0,0, sin(a),cos(a),0,0, 0,0,1,0, 0,0,0,1]
+  let mult = (A,B)=> [(A)[0]*(B)[0]+(A)[1]*(B)[4]+(A)[2]*(B)[8]+(A)[3]*(B)[12],(A)[0]*(B)[1]+(A)[1]*(B)[5]+(A)[2]*(B)[9]+(A)[3]*(B)[13],(A)[0]*(B)[2]+(A)[1]*(B)[6]+(A)[2]*(B)[10]+(A)[3]*(B)[14],(A)[0]*(B)[3]+(A)[1]*(B)[7]+(A)[2]*(B)[11]+(A)[3]*(B)[15],(A)[4]*(B)[0]+(A)[5]*(B)[4]+(A)[6]*(B)[8]+(A)[7]*(B)[12],(A)[4]*(B)[1]+(A)[5]*(B)[5]+(A)[6]*(B)[9]+(A)[7]*(B)[13],(A)[4]*(B)[2]+(A)[5]*(B)[6]+(A)[6]*(B)[10]+(A)[7]*(B)[14],(A)[4]*(B)[3]+(A)[5]*(B)[7]+(A)[6]*(B)[11]+(A)[7]*(B)[15],(A)[8]*(B)[0]+(A)[9]*(B)[4]+(A)[10]*(B)[8]+(A)[11]*(B)[12],(A)[8]*(B)[1]+(A)[9]*(B)[5]+(A)[10]*(B)[9]+(A)[11]*(B)[13],(A)[8]*(B)[2]+(A)[9]*(B)[6]+(A)[10]*(B)[10]+(A)[11]*(B)[14],(A)[8]*(B)[3]+(A)[9]*(B)[7]+(A)[10]*(B)[11]+(A)[11]*(B)[15],(A)[12]*(B)[0]+(A)[13]*(B)[4]+(A)[14]*(B)[8]+(A)[15]*(B)[12],(A)[12]*(B)[1]+(A)[13]*(B)[5]+(A)[14]*(B)[9]+(A)[15]*(B)[13],(A)[12]*(B)[2]+(A)[13]*(B)[6]+(A)[14]*(B)[10]+(A)[15]*(B)[14],(A)[12]*(B)[3]+(A)[13]*(B)[7]+(A)[14]*(B)[11]+(A)[15]*(B)[15]]
+  let trfm = (A,v)=> [((A)[0]*(v)[0]+(A)[1]*(v)[1]+(A)[2]*(v)[2]+(A)[3])/((A)[12]*(v)[0]+(A)[13]*(v)[1]+(A)[14]*(v)[2]+(A)[15]),((A)[4]*(v)[0]+(A)[5]*(v)[1]+(A)[6]*(v)[2]+(A)[7])/((A)[12]*(v)[0]+(A)[13]*(v)[1]+(A)[14]*(v)[2]+(A)[15]),((A)[8]*(v)[0]+(A)[9]*(v)[1]+(A)[10]*(v)[2]+(A)[11])/((A)[12]*(v)[0]+(A)[13]*(v)[1]+(A)[14]*(v)[2]+(A)[15])]
+  let proj = (f,v)=> [(f)*(v)[0]/(v)[2],(f)*(v)[1]/(v)[2]];
+  
+  let T = mult([1,0,0,dx, 0,1,0,dy, 0,0,1,dz, 0,0,0,1],mult(rotz(rz),mult(roty(ry),rotx(rx))));
+
+  return proj(f,trfm(T,[x,y,z]));
+}
+
+
 // update our data everytime the server sends us an update
 socket.on('connection-approve', function(data){
   status = "approve";
@@ -33,13 +47,62 @@ function setup() {
 
 
 function draw() {
+  let hW = width;
+  let hH = height/2;
+  
   noStroke();
   fill(...colors[0])
-  rect(0,0,width,height/2);
+  if (height>width){
+    rect(0,0,width,height/2);
+  }else{
+    
+  }
+  rect(0,0,hW,hH);
   
-  translate(0,height/2);
   fill(...colors[1])
-  rect(0,0,width,height/2);
+  if (height>width){
+    translate(0,hH);
+  }else{
+    translate(hW,0);
+  }
+  rect(0,0,hW,hH);
+
+  push();
+  translate(width/2,height/4);
+  var cam = [-radians(rotationY),-radians(rotationX),-radians(rotationZ), 0,0,20, 100];
+  stroke(255);
+  strokeWeight(2);
+  let [va,vb,vc,vd, ve,vf,vg,vh] = [
+    vertex3d(-8,-8,-8, ...cam),
+    vertex3d( 8,-8,-8, ...cam),
+    vertex3d( 8, 8,-8, ...cam),
+    vertex3d(-8, 8,-8, ...cam),
+    
+    vertex3d(-8,-8, 8, ...cam),
+    vertex3d( 8,-8, 8, ...cam),
+    vertex3d( 8, 8, 8, ...cam),
+    vertex3d(-8, 8, 8, ...cam),
+  ]
+  line(...va,...vb);
+  line(...vb,...vc);
+  line(...vc,...vd);
+  line(...vd,...va);
+  
+  line(...ve,...vf);
+  line(...vf,...vg);
+  line(...vg,...vh);
+  line(...vh,...ve);
+  
+  line(...va,...ve);
+  line(...vb,...vf);
+  line(...vc,...vg);
+  line(...vd,...vh);
+  pop();
   
   
+}
+
+
+function windowResized() { //this detects when the window is resized, such as entering fullscreen mode, or changing orientation of the device.
+  resizeCanvas(windowWidth, windowHeight); //resizes the canvas to the new dimensions 
 }

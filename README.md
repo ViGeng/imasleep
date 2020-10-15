d276aaae852c67db2a6bb875b12ab9fdbbc988d7
Glitch (networked-hand-tf174-handv1) <none>
2020-10-15 02:47:39 +0000

    🎖🎥 Checkpoint ./public/sketch.js:1704233/10387 ./README.md:1704233/213

--- README.md
+++ README.md
@@ -1,15 +1,14 @@
-# sensors-duo
+# networked-touches
 
-- Share phone sensor data between exactly two players 
-- For simlicity, if a thrird (or more) player try to connect, they will basically be shut out (Check sensors-rooms for a more sophisticated solution)
+- Share multitouch data between exactly two players 
 
 ### public/sketch.js
 
-This is just like the normal "sketch.js" you usually have in p5.js projects, but it also includes code that talk to the server.
+This is just like the normal "sketch.js" you usually have in p5.js projects, but it also includes code that talks to the server.
 
 ### server.js
 
-This is the server code, which receives client's data and send them to other clients
+This is the server code, which receives client's data and send them to other client(s)
 
 
 ## Technologies
--- public/sketch.js
+++ public/sketch.js
@@ -1,3 +1,13 @@
+//------------------------------------------------
+// Don't delete these 'comments'; they are necessary to make p5.js work with Glitch.com.
+// First of all, shut glitch up about p5's global namespace pollution using this magic comment:
+/* global describe p5 setup draw P2D WEBGL ARROW CROSS HAND MOVE TEXT WAIT HALF_PI PI QUARTER_PI TAU TWO_PI DEGREES RADIANS DEG_TO_RAD RAD_TO_DEG CORNER CORNERS RADIUS RIGHT LEFT CENTER TOP BOTTOM BASELINE POINTS LINES LINE_STRIP LINE_LOOP TRIANGLES TRIANGLE_FAN TRIANGLE_STRIP QUADS QUAD_STRIP TESS CLOSE OPEN CHORD PIE PROJECT SQUARE ROUND BEVEL MITER RGB HSB HSL AUTO ALT BACKSPACE CONTROL DELETE DOWN_ARROW ENTER ESCAPE LEFT_ARROW OPTION RETURN RIGHT_ARROW SHIFT TAB UP_ARROW BLEND REMOVE ADD DARKEST LIGHTEST DIFFERENCE SUBTRACT EXCLUSION MULTIPLY SCREEN REPLACE OVERLAY HARD_LIGHT SOFT_LIGHT DODGE BURN THRESHOLD GRAY OPAQUE INVERT POSTERIZE DILATE ERODE BLUR NORMAL ITALIC BOLD BOLDITALIC LINEAR QUADRATIC BEZIER CURVE STROKE FILL TEXTURE IMMEDIATE IMAGE NEAREST REPEAT CLAMP MIRROR LANDSCAPE PORTRAIT GRID AXES frameCount deltaTime focused cursor frameRate getFrameRate setFrameRate noCursor displayWidth displayHeight windowWidth windowHeight width height fullscreen pixelDensity displayDensity getURL getURLPath getURLParams pushStyle popStyle popMatrix pushMatrix registerPromisePreload camera perspective ortho frustum createCamera setCamera setAttributes createCanvas resizeCanvas noCanvas createGraphics blendMode noLoop loop push pop redraw applyMatrix resetMatrix rotate rotateX rotateY rotateZ scale shearX shearY translate arc ellipse circle line point quad rect square triangle ellipseMode noSmooth rectMode smooth strokeCap strokeJoin strokeWeight bezier bezierDetail bezierPoint bezierTangent curve curveDetail curveTightness curvePoint curveTangent beginContour beginShape bezierVertex curveVertex endContour endShape quadraticVertex vertex alpha blue brightness color green hue lerpColor lightness red saturation background clear colorMode fill noFill noStroke stroke erase noErase createStringDict createNumberDict storeItem getItem clearStorage removeItem select selectAll removeElements createDiv createP createSpan createImg createA createSlider createButton createCheckbox createSelect createRadio createColorPicker createInput createFileInput createVideo createAudio VIDEO AUDIO createCapture createElement deviceOrientation accelerationX accelerationY accelerationZ pAccelerationX pAccelerationY pAccelerationZ rotationX rotationY rotationZ pRotationX pRotationY pRotationZ pRotateDirectionX pRotateDirectionY pRotateDirectionZ turnAxis setMoveThreshold setShakeThreshold isKeyPressed keyIsPressed key keyCode keyIsDown movedX movedY mouseX mouseY pmouseX pmouseY winMouseX winMouseY pwinMouseX pwinMouseY mouseButton mouseIsPressed requestPointerLock exitPointerLock touches createImage saveCanvas saveGif saveFrames loadImage image tint noTint imageMode pixels blend copy filter get loadPixels set updatePixels loadJSON loadStrings loadTable loadXML loadBytes httpGet httpPost httpDo createWriter save saveJSON saveJSONObject saveJSONArray saveStrings saveTable writeFile downloadFile abs ceil constrain dist exp floor lerp log mag map max min norm pow round sq sqrt fract createVector noise noiseDetail noiseSeed randomSeed random randomGaussian acos asin atan atan2 cos sin tan degrees radians angleMode textAlign textLeading textSize textStyle textWidth textAscent textDescent loadFont text textFont append arrayCopy concat reverse shorten shuffle sort splice subset float int str boolean byte char unchar hex unhex join match matchAll nf nfc nfp nfs split splitTokens trim day hour minute millis month second year plane box sphere cylinder cone ellipsoid torus orbitControl debugMode noDebugMode ambientLight specularColor directionalLight pointLight lights lightFalloff spotLight noLights loadModel model loadShader createShader shader resetShader normalMaterial texture textureMode textureWrap ambientMaterial emissiveMaterial specularMaterial shininess remove canvas drawingContext*/
+// Also socket.io:
+/* global describe io*/
+// Now any other lint errors will be your own problem.
+
+
+//------------------------------------------------
 // Demo in which two mobile phones share p5.js "touches" 
 // events (multi-touch finger positions).
 // See https://p5js.org/reference/#/p5/touches
@@ -11,6 +21,28 @@ var serverData = {}; // stores other users's data from the server
 var status = "unknown"; // or 'approve', or 'reject', depending on whether the server decides to let you in
 
 
+//------------------------------------------------
+// Event handlers for the Socket library. 
+// You probably won't need to change these. 
+//
+socket.on('connection-approve', function(data){
+  // Update status when server tells us when 
+  // they approve our request to join a room
+  status = "approve";
+})
+socket.on('connection-reject', function(data){
+  // Update status when server tells us when 
+  // they reject our request to join a room
+  status = "reject";
+})
+socket.on('server-update',function(data){
+  // Update our copy eof the other player's data
+  // everytime the server sends us an update
+  serverData = data;
+})
+
+
+
 // RGB color backgrounds for the two players
 var colors = [
   [120,200,255],
@@ -21,7 +53,7 @@ var colors = [
 //------------------------------------------------
 // The main p5.js setup
 function setup() {
-  createCanvas(window.innerWidth,window.innerHeight);  
+  createCanvas(windowWidth, windowHeight);
 }
 
 //------------------------------------------------
@@ -88,6 +120,11 @@ function showMyErrorScreen(msg){
 // https://p5js.org/reference/#/p5/touchStarted
 //
 function touchStarted(){
+  var fs = fullscreen();
+  if (!fs) {
+    // fullscreen(true);
+  }
+  
   return false;
 }
 function touchMoved(){
@@ -97,35 +134,19 @@ function touchEnded() {
   return false;
 }
 
+/* prevents the mobile browser from processing some default
+ * touch events, like swiping left for "back" or scrolling
+ * the page.
+
+document.ontouchmove = function(event) {
+    event.preventDefault();
+};
+ */
+
 function windowResized() { //this detects when the window is resized, such as entering fullscreen mode, or changing orientation of the device.
   resizeCanvas(windowWidth, windowHeight); //resizes the canvas to the new dimensions 
 }
 
-//------------------------------------------------
-// Event handlers for the Socket library. 
-// You probably won't need to change these. 
-//
-socket.on('connection-approve', function(data){
-  // Update status when server tells us when 
-  // they approve our request to join a room
-  status = "approve";
-})
-socket.on('connection-reject', function(data){
-  // Update status when server tells us when 
-  // they reject our request to join a room
-  status = "reject";
-})
-socket.on('server-update',function(data){
-  // Update our copy eof the other player's data
-  // everytime the server sends us an update
-  serverData = data;
-})
 
 
-//------------------------------------------------
-// Don't delete these 'comments'; they are necessary to make p5.js work with Glitch.com.
-// First of all, shut glitch up about p5's global namespace pollution using this magic comment:
-/* global describe p5 setup draw P2D WEBGL ARROW CROSS HAND MOVE TEXT WAIT HALF_PI PI QUARTER_PI TAU TWO_PI DEGREES RADIANS DEG_TO_RAD RAD_TO_DEG CORNER CORNERS RADIUS RIGHT LEFT CENTER TOP BOTTOM BASELINE POINTS LINES LINE_STRIP LINE_LOOP TRIANGLES TRIANGLE_FAN TRIANGLE_STRIP QUADS QUAD_STRIP TESS CLOSE OPEN CHORD PIE PROJECT SQUARE ROUND BEVEL MITER RGB HSB HSL AUTO ALT BACKSPACE CONTROL DELETE DOWN_ARROW ENTER ESCAPE LEFT_ARROW OPTION RETURN RIGHT_ARROW SHIFT TAB UP_ARROW BLEND REMOVE ADD DARKEST LIGHTEST DIFFERENCE SUBTRACT EXCLUSION MULTIPLY SCREEN REPLACE OVERLAY HARD_LIGHT SOFT_LIGHT DODGE BURN THRESHOLD GRAY OPAQUE INVERT POSTERIZE DILATE ERODE BLUR NORMAL ITALIC BOLD BOLDITALIC LINEAR QUADRATIC BEZIER CURVE STROKE FILL TEXTURE IMMEDIATE IMAGE NEAREST REPEAT CLAMP MIRROR LANDSCAPE PORTRAIT GRID AXES frameCount deltaTime focused cursor frameRate getFrameRate setFrameRate noCursor displayWidth displayHeight windowWidth windowHeight width height fullscreen pixelDensity displayDensity getURL getURLPath getURLParams pushStyle popStyle popMatrix pushMatrix registerPromisePreload camera perspective ortho frustum createCamera setCamera setAttributes createCanvas resizeCanvas noCanvas createGraphics blendMode noLoop loop push pop redraw applyMatrix resetMatrix rotate rotateX rotateY rotateZ scale shearX shearY translate arc ellipse circle line point quad rect square triangle ellipseMode noSmooth rectMode smooth strokeCap strokeJoin strokeWeight bezier bezierDetail bezierPoint bezierTangent curve curveDetail curveTightness curvePoint curveTangent beginContour beginShape bezierVertex curveVertex endContour endShape quadraticVertex vertex alpha blue brightness color green hue lerpColor lightness red saturation background clear colorMode fill noFill noStroke stroke erase noErase createStringDict createNumberDict storeItem getItem clearStorage removeItem select selectAll removeElements createDiv createP createSpan createImg createA createSlider createButton createCheckbox createSelect createRadio createColorPicker createInput createFileInput createVideo createAudio VIDEO AUDIO createCapture createElement deviceOrientation accelerationX accelerationY accelerationZ pAccelerationX pAccelerationY pAccelerationZ rotationX rotationY rotationZ pRotationX pRotationY pRotationZ pRotateDirectionX pRotateDirectionY pRotateDirectionZ turnAxis setMoveThreshold setShakeThreshold isKeyPressed keyIsPressed key keyCode keyIsDown movedX movedY mouseX mouseY pmouseX pmouseY winMouseX winMouseY pwinMouseX pwinMouseY mouseButton mouseIsPressed requestPointerLock exitPointerLock touches createImage saveCanvas saveGif saveFrames loadImage image tint noTint imageMode pixels blend copy filter get loadPixels set updatePixels loadJSON loadStrings loadTable loadXML loadBytes httpGet httpPost httpDo createWriter save saveJSON saveJSONObject saveJSONArray saveStrings saveTable writeFile downloadFile abs ceil constrain dist exp floor lerp log mag map max min norm pow round sq sqrt fract createVector noise noiseDetail noiseSeed randomSeed random randomGaussian acos asin atan atan2 cos sin tan degrees radians angleMode textAlign textLeading textSize textStyle textWidth textAscent textDescent loadFont text textFont append arrayCopy concat reverse shorten shuffle sort splice subset float int str boolean byte char unchar hex unhex join match matchAll nf nfc nfp nfs split splitTokens trim day hour minute millis month second year plane box sphere cylinder cone ellipsoid torus orbitControl debugMode noDebugMode ambientLight specularColor directionalLight pointLight lights lightFalloff spotLight noLights loadModel model loadShader createShader shader resetShader normalMaterial texture textureMode textureWrap ambientMaterial emissiveMaterial specularMaterial shininess remove canvas drawingContext*/
-// Also socket.io:
-/* global describe io*/
-// Now any other lint errors will be your own problem.
+

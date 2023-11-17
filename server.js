eb1ad183d4f333ae42937efc03ee494c98231529
Glitch (sleep-early-together) <none>
2023-11-17 09:23:37 +0000

    🏇👛 Checkpoint ./public/sketch.js copy:62999044/8908 ./public/sketch.js:62999044/17925 ./server.js:62999044/138

--- server.js
+++ server.js
@@ -7,7 +7,7 @@
 // to deal with a received message: socket.on(title,function(data){ frob(data); })
 
 //========================================================================
-const MAX_PLAYERS = 20; // maximum number of players, which is 2 by default.
+const MAX_PLAYERS = 2; // maximum number of players, which is 2 by default.
                        // you can freely change it to another number here,
                        // but will need to update the drawing code in public/sketch.js accordingly
 
@@ -61,13 +61,14 @@ function newConnection(socket){
   // the data of everybody else's client.
   // Note: setInterval(f,t) = runs function f every t milliseconds
   let timer = setInterval(function(){
-    let others = {};
-    for (let k in serverData){
-      if (k != socket.id){
-        others[k] = serverData[k];
-      }
-    }
-		socket.emit('server-update', others);
+    // let others = {};
+    // for (let k in serverData){
+    //   if (k != socket.id){
+    //     others[k] = serverData[k];
+    //   }
+    // }
+		// socket.emit('server-update', others);
+    socket.emit('server-update', serverData);
 	}, 2000);
   
   
